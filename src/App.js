bbefe7000a8593a07a6d5044a57d9d4e161dfa61
XTiNCT-7 <iamram48@gmail.com>
2021-08-09 12:01:12 +0530

    changed tab icon

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <>
     <Router>
-       <NavMenu />
+      <NavMenu />
     </Router>     
-    </>
   );
 }
 
