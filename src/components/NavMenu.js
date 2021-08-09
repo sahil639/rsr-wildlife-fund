c6ba5ad228125ec8ec8420caed3c5973854107ab
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-08-09 10:07:04 +0530

    updated by sahill

---
+++ src/components/NavMenu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function NavMenu() {
+    return (
+        <div>
+            <h1>
+                bocha1
+            </h1>
+            <h1>
+                bocha2
+            </h1>
+            <h1>
+                bocha3
+            </h1>
+        </div>
+    )
+}
+
+export default NavMenu
