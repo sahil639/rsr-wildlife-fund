3bf05ae6c78931676f2de1ba07f268cb1df26760
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-09-28 00:06:18 +0530

    lil changes

--- src/components/Founders.js
+++ src/components/Founders.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Founders() {
     return (
         <div>
-            
+            <div className="founder-section">
+            <h1>Fight World Hunger</h1>
+            </div>
         </div>
     )
 }
